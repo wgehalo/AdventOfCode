780c17b50b49695f478e47f4cbaa46ace40c9712
gehabr01 <brian.gehalo@lexisnexisrisk.com>
2018-12-04 11:45:00 -0500

    Cleaned up, started using sets and added timer.

--- Day 3/Part2.py
+++ Day 3/Part2.py
@@ -1,54 +1,50 @@
 import re
+import timeit
+
+start = timeit.default_timer()
+
+all_claims = set()
+overlapped_claims = set()
+overlap_count = 0
+
 
 def create_matrix(x, y):
-   return [[0 for y in range(y)] for x in range(x)]
+    return [[[0, 0] for y in range(y)] for x in range(x)]
+
 
 def add_rectangle(x, y, width, height, id):
     global overlap_count
-    global no_overlap
-    overlapped = False
+    all_claims.add(id)
     for x_n in range(x, x + width):
         for y_n in range(y, y + height):
             current_square = fabric[y_n][x_n]
-            if current_square == 0:
-                fabric[y_n][x_n] = 1
-            elif current_square == 1:
+            if current_square[0] == 0:
+                fabric[y_n][x_n] = [1, id]
+            elif current_square[0] == 1:
                 overlap_count = overlap_count + 1
-                fabric[y_n][x_n] = 2
-            elif current_square == 2:
-                overlapped = True
-    if not overlapped:
-        no_overlap = id
+                overlapped_claims.add(id)
+                overlapped_claims.add(current_square[1])
+                fabric[y_n][x_n] = [2, id]
+            else:
+                overlapped_claims.add(id)
 
-overlap_count = 0
-fabric = create_matrix(1000, 1000)
-no_overlap = ''
 
 with open('Day 3/input.txt') as f:
-    line = f.readline()
-    while line:
-        parsed = re.search(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', line)
+    fabric = create_matrix(1000, 1000)
+    lines = f.readlines()
+    for line in lines:
+        parsed = re.search(r'^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$', line)
         id = parsed.group(1)
         x = int(parsed.group(2))
         y = int(parsed.group(3))
         width = int(parsed.group(4))
         height = int(parsed.group(5))
         add_rectangle(x, y, width, height, id)
-        line = f.readline()
 
     print(f'Total overlapping square: {overlap_count}')
-    f.seek(0)
-    line = f.readline()
-    while line:
-        parsed = re.search(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', line)
-        id = parsed.group(1)
-        x = int(parsed.group(2))
-        y = int(parsed.group(3))
-        width = int(parsed.group(4))
-        height = int(parsed.group(5))
-        add_rectangle(x, y, width, height, id)
-        line = f.readline()
+    print(f'This one did not overlap: {all_claims - overlapped_claims}')
 
+stop = timeit.default_timer()
+total_time = stop - start
 
-
-print(f'This one did not overlap: {no_overlap}')
+print(f'Total Time: {total_time}')
