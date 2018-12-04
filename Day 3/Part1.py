30c695493d76a79a503e76b2d5ab741dcf0e0041
wgehalo <wgehalo@gmail.com>
2018-12-03 21:28:36 -0500

    Day 3 part 1 working.

--- Day 3/Part1.py
+++ Day 3/Part1.py
@@ -1,33 +1,21 @@
 import re
-# Initialize the array
-overlap_count = 0
-
 
 def create_matrix(x, y):
-    matrix = [[0 for y in range(y)] for x in range(x)]
-
-    # matrix = []
-    # for n in range(0, x):
-    #     row = []
-    #     for n_2 in range(0, y):
-    #         row.append(0)
-    #     matrix.append(row)
-    return matrix
-
+   return [[0 for y in range(y)] for x in range(x)]
 
 def add_rectangle(x, y, width, height):
     global overlap_count
-    for x_n in range(x, x + width + 1):
-        for y_n in range(y, y + height + 1):
-            current_square = fabric[x_n][y_n]
+    for x_n in range(x, x + width):
+        for y_n in range(y, y + height):
+            current_square = fabric[y_n][x_n]
             if current_square == 0:
-                fabric[x_n][y_n] = 1
+                fabric[y_n][x_n] = 1
             elif current_square == 1:
                 overlap_count = overlap_count + 1
-                fabric[x_n][y_n] = 2
+                fabric[y_n][x_n] = 2
 
-
-fabric = create_matrix(1001, 1001)
+overlap_count = 0
+fabric = create_matrix(1000, 1000)
 
 with open('Day 3/input.txt') as f:
     line = f.readline()
@@ -39,6 +27,5 @@ with open('Day 3/input.txt') as f:
         height = int(parsed.group(4))
         add_rectangle(x, y, width, height)
         line = f.readline()
-        # 1 @ 935,649: 22x22
 
 print(overlap_count)
